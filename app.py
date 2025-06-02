417698b9f1488b5509007e74efae45d8455e5a70
Gabriel <gbernardo21@gmail.com>
2025-06-02 09:52:20 -0300

    Adicionado filtro para mensagem de status

--- app.py
+++ app.py
@@ -12,38 +12,24 @@ app = Flask(__name__)
 @app.route('/chatbot/webhook/', methods=['POST'])
 def webhook():
     data = request.json
+    chat_id = data['payload']['from']
+    received_message = data['payload']['body']
+    is_group = '@g.us' in chat_id
+    is_status = 'status@broadcast'in chat_id
 
-    print(f'EVENTO RECEBIDO: {data}')
-
+    if is_group or is_status:
+        return jsonify({'status': 'success', 'message': 'Mensagem de grupo/status ignorada.'}), 200
+    
     waha = Waha()
     ai_bot = AIBot()
 
-    chat_id = data['payload']['from']
-    # --- INÍCIO DA DEPURACÃO MAIS DETALHADA ---
-    print(f"DEBUG: chat_id extraído: '{chat_id}'")
-    print(f"DEBUG: Tipo de chat_id: {type(chat_id)}")
-    print(f"DEBUG: O chat_id contém '@g.us'? {'@g.us' in chat_id}")
-    # --- FIM DA DEPURACÃO MAIS DETALHADA ---
-    # Verifica se o chat_id indica um grupo (contém '@g.us')
-    if '@g.us' in chat_id:
-        print('DEBUG: Condição de grupo VERDADEIRA.')
-        print('Mensagem recebida de um grupo ({chat_id}). Ignorando resposta.')
-        return jsonify({'status': 'ignored_group_message'}), 200
-    print('DEBUG: Condição de grupo FALSA. Prosseguindo com a resposta.')
-    # Esta linha só deve aparecer se não for grupo.
-
-    received_message = data['payload']['body']
-
     waha.start_typing(chat_id=chat_id)
-
     response = ai_bot.invoke(question=received_message)
     sleep(randint(1, 2))
-
     waha.send_message(
         chat_id=chat_id,
         message=response,
     )
-
     waha.stop_typing(chat_id=chat_id)
 
     return jsonify({'status': 'success'}), 200
